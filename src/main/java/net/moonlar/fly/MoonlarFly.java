cb8037dcac8809b96eb0e25cc70d7268fdbb78ee
GiverPlay007 <play.giver.007@gmail.com>
2021-10-25 22:00:45 -0300

    /fly command

--- src/main/java/net/moonlar/fly/MoonlarFly.java
+++ src/main/java/net/moonlar/fly/MoonlarFly.java
@@ -1,16 +1,34 @@
 package net.moonlar.fly;
 
+import net.moonlar.fly.commands.FlyCommand;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MoonlarFly extends JavaPlugin {
 
   @Override
   public void onEnable() {
-
+    getCommand("fly").setExecutor(new FlyCommand(this));
   }
 
   @Override
   public void onDisable() {
 
+  }
+
+  public void toggleFly(Player player) {
+    if(player.getAllowFlight()) {
+      player.setAllowFlight(false);
+      player.sendMessage("&cModo fly desativado!");
+    }
+    else {
+      player.setAllowFlight(true);
+      player.sendMessage("&aModo fly ativado!");
+    }
+  }
+
+  public boolean hasPermission(CommandSender sender, String permission) {
+    return sender.hasPermission("moonlar.fly.admin") || sender.hasPermission(permission);
   }
 }
---
+++ src/main/java/net/moonlar/fly/commands/FlyCommand.java
@@ -0,0 +1,57 @@
+package net.moonlar.fly.commands;
+
+import net.moonlar.fly.MoonlarFly;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FlyCommand implements CommandExecutor {
+
+  private final MoonlarFly plugin;
+
+  public FlyCommand(MoonlarFly plugin) {
+    this.plugin = plugin;
+  }
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if(!plugin.hasPermission(sender, "moonlar.fly")) {
+      sender.sendMessage("&cVocê não tem permissão para voar.");
+      return true;
+    }
+
+    if(args.length == 0 && sender instanceof Player) {
+      plugin.toggleFly((Player) sender);
+      return true;
+    }
+
+    if(args.length == 1) {
+      if(!plugin.hasPermission(sender, "moonlar.fly.other")) {
+        sender.sendMessage("&cvocê não pode alterar o fly de outro jogador!");
+        return true;
+      }
+
+      Player player = Bukkit.getPlayer(args[0]);
+
+      if(player == null) {
+        sender.sendMessage("&cJogador não encontrado!");
+        return true;
+      }
+
+      plugin.toggleFly(player);
+
+      if(player.getAllowFlight()) {
+        sender.sendMessage("&aModo fly ativado para " + player.getName() + "!");
+        return true;
+      }
+
+      sender.sendMessage("&cModo fly desativado para " + player.getName() + "!");
+      return true;
+    }
+
+    sender.sendMessage("&cUtilize: /fly <nickname>");
+    return true;
+  }
+}
