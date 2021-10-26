c635d8d4b35ac7787595aa152b5b2e035cd425ad
GiverPlay007 <play.giver.007@gmail.com>
2021-10-25 21:35:45 -0300

    Initialize project

---
+++ src/main/java/net/moonlar/fly/MoonlarFly.java
@@ -0,0 +1,16 @@
+package net.moonlar.fly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MoonlarFly extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
